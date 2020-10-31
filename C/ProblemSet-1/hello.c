0e867ae53e5e63c3da73372aaa1d68da39f23eb0
nsoto92 <nor.soto92@gmail.com>
2020-10-30 19:08:33 -0700

    added hello.c

---
+++ C/ProblemSet-1/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    //Create name variable that takes user input
+    string name = get_string("What is your name?");
+    //Print greeting to user using name variable
+    printf("hello, %s\n", name);
+}
